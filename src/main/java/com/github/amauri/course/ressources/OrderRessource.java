a7c0481224b5eb84daa79ba22962d9ebdb78d88a
amauri <galete.amauri@gmail.com>
2021-04-10 16:48:09 -0300

    Payment, one-to-one association

--- src/main/java/com/github/amauri/course/ressources/OrderRessource.java
+++ src/main/java/com/github/amauri/course/ressources/OrderRessource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.amauri.course.entities.Order;
-import com.github.amauri.course.entities.Order;
 import com.github.amauri.course.services.OrderService;
 
 @RestController
