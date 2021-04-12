8ebcded48311a197608935508dd879b1a559c61f
amauri <galete.amauri@gmail.com>
2021-04-12 12:10:55 -0300

    Exception handling - findById

--- src/main/java/com/github/amauri/course/services/UserService.java
+++ src/main/java/com/github/amauri/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.github.amauri.course.entities.User;
 import com.github.amauri.course.repositories.UserRepository;
+import com.github.amauri.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);		
-		return obj.get();		
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));		
 	}	
 	
 	public User insert(User obj) {
